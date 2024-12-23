c724cdea50bf1946fc60c77050299c39f212d946
Triveni-2001 <trivenimali2001@gmail.com>
2024-12-23 17:31:03 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -14,15 +14,23 @@ function Navbar() {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="static" style={{ backgroundColor: '#FFA07A' }}>
       <Toolbar>
-        <Typography variant="h6" style={{ flexGrow: 1 }}>
+        <Typography variant="h6" style={{ flexGrow: 1, color: '#FFF' }}>
           Event Management Dashboard
         </Typography>
-        <Button color="inherit" component={Link} to="/">Events</Button>
-        <Button color="inherit" component={Link} to="/attendees">Attendees</Button>
-        <Button color="inherit" component={Link} to="/tasks">Tasks</Button>
-        <Button color="inherit" onClick={handleLogout}>Logout</Button>
+        <Button color="inherit" component={Link} to="/" style={{ color: '#FFF' }}>
+          Events
+        </Button>
+        <Button color="inherit" component={Link} to="/attendees" style={{ color: '#FFF' }}>
+          Attendees
+        </Button>
+        <Button color="inherit" component={Link} to="/tasks" style={{ color: '#FFF' }}>
+          Tasks
+        </Button>
+        <Button color="inherit" onClick={handleLogout} style={{ color: '#FFF' }}>
+          Logout
+        </Button>
       </Toolbar>
     </AppBar>
   );
